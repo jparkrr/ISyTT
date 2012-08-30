c5e04957cc40613c7dfe442d7e757fb0619d555f
Charlie Johnson <cjohnson415@gmail.com>
2012-08-30 11:55:14 -0700

    share

--- scripts/share.js
+++ scripts/share.js
@@ -1,11 +1,17 @@
-var async = require('async');
 var SINGLY = 'https://api.singly.com/';
-var request = require('request');
+var request = require('../node_modules/request');
 
 exports.run = function(update, token, services) {
-  var url =  SINGLY + 'types/:type?access_token=' + token + '&services=' + services.join(',');
-  request.post({url: url, body: update});
+  var url =  SINGLY + 'types/statuses?access_token=' + token + '&services=' + services.join(',');
+  url += '&body=' + update;
+  request.post(url, function(err, resp){
+    console.log(err);
+    console.log(resp.body);
+  });
 }
-var token = 'bRMdRFwevENIh524ZT_VxqOizh4=eRlG7G0j56e1e52ffa7efe56edb309cf28a269cc8e13546026b71738c9cd3ef3e76c11e4bbbc7d7d0b194ec5f3fbefd3a1013b01d9dc4dc0dba47f5eeeb6b6d0b99893da939abe49fd51841c6b1a7f43f52756aecb68588dae9a6799227b8ed8240e93ef';
 
-run("tester", token, ['facebook', 'linkedin', 'yammer']); 
+
+//Testing
+var token = '1lrhqlmh8DQAofTO7xTZ2m6t194=DXXU2JsO10b2b790855e848b3f630fafe18e1cc13a1138b35e1bc953b514d9608dd76ce73fa62fe5b6e0d5a5e1526ad594e8452cd3c171d29827f114082cea607560ea535bfa04c3ea2c0ec3dd3ac5b973779e447186cbb98d172848814cbf8238dcc1c8193a35d8d51b7dbc4ba83eb9ba3dd530';
+
+exports.run("tester", token, ['facebook', 'linkedin', 'yammer']); 
---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express.createServer();
+
+app.post('/event', function(req, res) {
+
+  
+});
+
+
