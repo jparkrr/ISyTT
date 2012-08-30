0b5e38cf5dd489f56f83ad5754aa84d03d4b75aa
Charlie Johnson <cjohnson415@gmail.com>
2012-08-30 16:11:27 -0700

    Framework for ifs and thens
    I want to reorganize parameter calling to occur only in scripts,
    i.e. no specifics of the then are in server.js, but rather the scripts
    call general functions in lib.js.

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var server = express();
 require('./auth')(server, clientId, clientSecret, hostBaseUrl, hostPort);
 
 // POSTman to catch scripts and run posts
-var token = '1lrhqlmh8DQAofTO7xTZ2m6t194=DXXU2JsO10b2b790855e848b3f630fafe18e1cc13a1138b35e1bc953b514d9608dd76ce73fa62fe5b6e0d5a5e1526ad594e8452cd3c171d29827f114082cea607560ea535bfa04c3ea2c0ec3dd3ac5b973779e447186cbb98d172848814cbf8238dcc1c8193a35d8d51b7dbc4ba83eb9ba3dd530';
 
 server.get('/share', function(req, res) {
   THEN.share('testnow', token, ['facebook']);
@@ -22,6 +21,5 @@ server.get('/share', function(req, res) {
 
 server.post('/event', function(req, res) { 
   var body = req.body;
-  var token;
-  if (IF.atWork(body)) THEN.share(['twitter'], "I'm at work!", token);
+  if (IF.friendStatus(body, ['Justin Parker', 'Charlie Johnson'])) THEN.textNotify('Your friend has a status update', '+14152053607');
 });
