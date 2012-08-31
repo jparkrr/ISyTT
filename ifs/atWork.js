2acbd9d5598565be92b05cfc43c16bdf1f1249fb
Charlie Johnson <cjohnson415@gmail.com>
2012-08-30 18:01:21 -0700

    fixes

--- ifs/atWork.js
+++ ifs/atWork.js
@@ -2,6 +2,6 @@ var work = require('../config/config.json').work;
 var lib = require('./lib.js');
 
 module.exports = function(data) {
-  if (!data || !data[0] || !data[0].type) return false;
+  if (!data || !data[0] || !data[0].type || data[0].type != 'checkin') return false;
   return lib.checkinIsNear(data[0].oembed, work, work.err); 
 };
--- ifs/lib.js
+++ ifs/lib.js
@@ -1,8 +1,6 @@
 module.exports.checkinIsNear = function(checkin, place, radius) {
-  if (!data || !data[0] || !data[0].type) return false;
-  var checkin = data[0].oembed;
   var ret = true;
-  if (checkin.lat < work.lat - work.err || checkin.lat > work.lat + work.err) ret = false;
-  if (checkin.lng < work.lng - work.err || checkin.lng > work.lng + work.err) ret = false;
+  if (checkin.lat < work.lat - radius || checkin.lat > work.lat + radius) ret = false;
+  if (checkin.lng < work.lng - radius || checkin.lng > work.lng + radius) ret = false;
   return ret;
 };
