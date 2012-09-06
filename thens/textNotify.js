689a8eb33795ee31a71a72ec50ce17d072d5f82d
Charlie Johnson <cjohnson415@gmail.com>
2012-09-05 18:06:30 -0700

    Properly fetch env variables

--- thens/textNotify.js
+++ thens/textNotify.js
@@ -1,6 +1,11 @@
 var request = require('request');
 var querystring = require('querystring');
 
+var TWILIO_SID = process.env.TWILIO_SID;
+var TWILIO_HOSTNAME = process.env.TWILIO_TOKEN;
+var TWILIO_HOSTNUMBER = process.env.TWILIO_HOSTNUMBER;
+var TWILIO_TOKEN = process.env.TWILIO_TOKEN;
+
 module.exports = function(message, to) {
   var twilio = {sid: TWILIO_SID, token: TWILIO_TOKEN,
                 hostname: TWILIO_HOSTNAME, hostnumber: TWILIO_HOSTNUMBER};
