ee5c9f55d09482b9040cd431d17b67eca5a70844
Charlie Johnson <cjohnson415@gmail.com>
2012-09-05 18:10:13 -0700

    more env variable errs

--- thens/textNotify.js
+++ thens/textNotify.js
@@ -11,8 +11,8 @@ module.exports = function(message, to) {
                 hostname: TWILIO_HOSTNAME, hostnumber: TWILIO_HOSTNUMBER};
   if (!message) message = 'You have a text notification without a message!';
   var TwilioClient = require('twilio').Client;
-  var client = new TwilioClient(twilio.sid, twilio.token, twilio.hostname);
-  var phone = client.getPhoneNumber(twilio.HOSTNUMBER);
+  var client = new TwilioClient(TWILIO_SID, TWILIO_TOKEN, TWILIO_HOSTNAME);
+  var phone = client.getPhoneNumber(TWILIO_HOSTNUMBER);
   phone.setup(function() {
     phone.sendSms(to, message, null, function(sms) {console.log(sms);});
   });
