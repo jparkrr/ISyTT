e4356eba6f65125ee62939b2dbd9778a64a2a328
Charlie Johnson <cjohnson415@gmail.com>
2012-09-05 17:59:16 -0700

    add testing logs

--- ifs/atWork.js
+++ ifs/atWork.js
@@ -1,7 +1,15 @@
 var work = require('../config/config.json').work;
-var lib = require('./lib.js');
 
 module.exports = function(data) {
+console.log('hello!');
   if (!data || !data[0] || !data[0].type || data[0].type != 'checkin') return false;
-  return lib.checkinIsNear(data[0].oembed, work, work.err); 
+  return checkinIsNear(data[0].oembed, work, work.err); 
+};
+
+
+checkinIsNear = function(checkin, place, radius) {
+  var ret = true;
+  if (checkin.lat < place.lat - radius || checkin.lat > place.lat + radius) ret = false;
+  if (checkin.lng < place.lng - radius || checkin.lng > place.lng + radius) ret = false;
+  return ret;
 };
